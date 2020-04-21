81a67c179365ec90b5d11c07b324f6d0ff2c4abc
bencarp <developer@bencarp.com>
2020-04-22 00:50:33 +0200

    session header bar mostly done, not styled yet

---
+++ public/javascripts/sharebuttons.js
@@ -0,0 +1,16 @@
+(function(r, d, s) {
+    r.loadSkypeWebSdkAsync = r.loadSkypeWebSdkAsync || function(p) {
+        var js, sjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(p.id)) { return; }
+        js = d.createElement(s);
+        js.id = p.id;
+        js.src = p.scriptToLoad;
+        js.onload = p.callback
+        sjs.parentNode.insertBefore(js, sjs);
+    };
+    var p = {
+        scriptToLoad: 'https://swx.cdn.skype.com/shared/v/latest/skypewebsdk.js',
+        id: 'skype_web_sdk'
+    };
+    r.loadSkypeWebSdkAsync(p);
+})(window, document, 'script');
