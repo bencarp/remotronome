d0b8126b40e735e750068d7dd95f418cfebd5728
bencarp <developer@bencarp.com>
2020-04-22 17:28:13 +0200

    Share button functionality

--- public/javascripts/sharebuttons.js
+++ public/javascripts/sharebuttons.js
@@ -1,3 +1,4 @@
+/* Skype share button */
 (function(r, d, s) {
     r.loadSkypeWebSdkAsync = r.loadSkypeWebSdkAsync || function(p) {
         var js, sjs = d.getElementsByTagName(s)[0];
@@ -14,3 +15,28 @@
     };
     r.loadSkypeWebSdkAsync(p);
 })(window, document, 'script');
+
+/* Copy to clipboard */
+function copyToClipboard() {
+    var dummy = document.createElement('input'),
+        text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
+
+/* System share (Web share API) */
+function systemShare() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Remotronome',
+            text: 'Please join me for a remote jam session at',
+            url: window.location.href,
+        })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+    }
+}
